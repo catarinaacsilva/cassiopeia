011c41f82361a2c0045226aa185d12c2fe49bed5
Catarina <c.alexandracorreia@ua.pt>
2021-03-19 15:17:38 +0000

    Add postgres on settings

--- cassiopeia/cassiopeia/settings.py
+++ cassiopeia/cassiopeia/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'NAME': 'cassiopeiadb',
         'USER': DATABASE_USERNAME,
         'PASSWORD': DATABASE_PASSWORD,
-        'HOST': 'postgres_cassiopeia',
+        'HOST': 'postgres',
         'PORT': '5432',
     }
 
