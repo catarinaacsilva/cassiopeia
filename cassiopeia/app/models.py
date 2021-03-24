21a5ae4e40372322b14187d614313ef7557976d3
Catarina <c.alexandracorreia@ua.pt>
2021-03-24 22:24:52 +0000

    Add autofield on models

--- cassiopeia/app/models.py
+++ cassiopeia/app/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-import uuid
 
 class Create_User(models.Model):
     email = models.EmailField(unique = True, null=False, primary_key=True)
@@ -10,18 +9,18 @@ class Create_User(models.Model):
     dateout = models.DateField()
 
 class Create_Policy(models.Model):
-    policyid = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    policyid = models.AutoField(primary_key=True)
     policy = models.CharField(max_length=1000, null = False)
 
 class Consent_Reply(models.Model):
-    consentid = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    consentid = models.AutoField(primary_key=True)
     timestamp = models.DateTimeField(auto_now_add = True)
     consent = models.BooleanField()
     email = models.ForeignKey(Create_User, on_delete=models.CASCADE)
     policyid = models.ForeignKey(Create_Policy, on_delete=models.CASCADE)
 
 class Device_Create(models.Model):
-    deviceid = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    deviceid = models.AutoField(primary_key=True)
     device = models.CharField(max_length=100)
     email = models.ForeignKey(Create_User, on_delete=models.CASCADE)
     policyid = models.ForeignKey(Create_Policy, on_delete=models.CASCADE)
