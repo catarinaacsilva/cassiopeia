f2d9299a5ca1416ae340349a712e971f47770906
Catarina <c.alexandracorreia@ua.pt>
2021-03-25 13:49:36 +0000

    Removed migrations

--- cassiopeia/cassiopeia/settings.py
+++ cassiopeia/cassiopeia/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'NAME': 'cassiopeiadb',
         'USER': DATABASE_USERNAME,
         'PASSWORD': DATABASE_PASSWORD,
-        'HOST': 'postgres',
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 
