7077018e46cee4aef1015f8b20a8e2de188f90e0
Catarina <c.alexandracorreia@ua.pt>
2021-08-19 13:39:38 +0100

    Add form  redirect

--- cassiopeia/static/js/form.js
+++ cassiopeia/static/js/form.js
@@ -4,13 +4,39 @@ function handleSubmit(event) {
   console.log(path);
   const data = new FormData(event.target);
   const values = Object.fromEntries(data.entries());
+  const body = JSON.stringify(values);
   console.log(values);
+
+  let redirect_url = null;
+  if ('redirect' in event.target) {
+    redirect_url = event.target['redirect'].value;
+  }
+   
+  console.log('redirect url: '+redirect_url);
+
   const xhr = new XMLHttpRequest();
   xhr.open("POST", path, true);
   xhr.setRequestHeader('Content-Type', 'application/json');
-  const body = JSON.stringify(values);
-  xhr.send(body);
+  xhr.timeout = 1000;
+
+  xhr.ontimeout = function () {
+    alert('Error('+xhr.status+'): '+xhr.statusText);
+    console.error('The request for ' + path + ' timed out.');
+  };
   
+  xhr.onload = function () {
+    if (xhr.readyState == 4) {
+      if (parseInt(xhr.status/100) == 2) {
+        console.log('OK:'+xhr.status);
+        if (redirect_url != null) {window.location.assign(redirect_url);}
+      } else {
+        alert('Error('+xhr.status+'): '+xhr.statusText);
+        console.error(xhr.statusText);
+      }
+    }
+  };
+
+  xhr.send(body);
 }
 
 console.log('Register all forms....')
